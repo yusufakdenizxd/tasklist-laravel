49bdad5d96a7b2a8604e67c1e983917aa77cd0e3
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2024-05-27 14:46:39 +0300

    redirect merhaba route added

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@ Route::get('/hello', function () {
     return 'Hello World';
 });
 
+Route::get('/merhaba', function () {
+    return redirect('/hello');
+});
+
 Route::get('/greetings/{name}', function ($name) {
     return 'Greetings ' . $name;
 });
