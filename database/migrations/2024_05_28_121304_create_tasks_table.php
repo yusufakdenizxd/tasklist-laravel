e0983ebf8d80bfbdcf357bbebd892e51f632baf7
Yusuf Akdeniz <yusufakdenizxd@gmail.com>
2024-05-28 16:42:49 +0300

    task factory, migration and seeder added for example data

--- database/migrations/2024_05_28_121304_create_tasks_table.php
+++ database/migrations/2024_05_28_121304_create_tasks_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');  
+            $table->text('long_description')->nullable();
+            $table->boolean('completed')->default(false);
             $table->timestamps();
         });
     }
